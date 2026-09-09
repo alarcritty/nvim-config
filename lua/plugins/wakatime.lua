e57a75a4ca796935b94dbc15b2a879f15eb710fc
alarcritty <adityamalik2833@gmail.com>
2026-09-09 15:21:07 +0530

    remove-wakatime

--- lua/plugins/wakatime.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "wakatime/vim-wakatime",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Force the plugin to initialize
-    vim.g.wakatime_PythonBinary = vim.fn.exepath('python3') or vim.fn.exepath('python')
-  end,
-}
